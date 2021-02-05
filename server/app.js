6e57daed99d4c346afc3db8c9252aeefc02db922
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-02-05 15:42:44 -0800

    Remove unnecessary server packages

--- server/app.js
+++ server/app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -12,10 +9,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 mongoose.connect(process.env.DB_URL, {
