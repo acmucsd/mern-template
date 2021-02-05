209c1fbeeb2ff206665a5710d3c16d44099e1f6b
Shravan Hariharan <shravanhariharan2@gmail.com>
2021-02-05 15:44:55 -0800

    Add dotenv loading

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const usersRouter = require('./routes/users');
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/users', usersRouter);
+
+dotenv.config();
 
 mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
