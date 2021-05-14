d9e71bcb16f9a96f10f13baf64cf9758d4118aff
mecruz322 <77129298+mecruz322@users.noreply.github.com>
2021-05-13 18:23:05 -0700

    header start

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Beateon</h1>
+          <button id = "sign-button">Sign up</button>
+          <button id = "log-button">Log in</button>
       </header>
     </div>
   );
