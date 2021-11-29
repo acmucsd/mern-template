f79d6e5944d070eca9098b1a580d3bead0b3971a
michaelliann <cafebubs@gmail.com>
2021-11-28 20:18:13 -0800

    Change schema passwordHash to password

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
         username: { type: String, required: true },
         name: { type: String, required: true },
         email: { type: String, required: true },
-        passwordHash: { type: String, required: true },
+        password: { type: String, required: true },
     },
     {
         timestamps: true,
