6fe58378992b72d813679bf9c5efc49d4ab44c00
michaelliann <cafebubs@gmail.com>
2021-11-28 18:13:49 -0800

    Added email to user schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema(
     {
         username: { type: String, required: true },
         name: { type: String, required: true },
+        email: { type: String, required: true },
         passwordHash: { type: String, required: true },
     },
     {
