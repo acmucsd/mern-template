85a7660f6bd14ac5522e67ed870f37876217de5e
dowhep <caogangshen@gmail.com>
2022-02-19 10:23:37 -0800

    cleaned up the environment variable usage

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,10 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const config = {
-    PORT: process.env.PORT,
-}
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+};
 
 module.exports = config;
