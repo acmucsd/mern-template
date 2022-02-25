762bd4f99454b6b5b2b94ccc80e20208c15e8e9c
Justin Phan <j1phan@ucsd.edu>
2022-02-25 12:41:02 -0800

    Routing for events/event pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import CreateAccount from './components/CreateAccount'
 import User from './components/User'
 import UserEvents from './components/UserEvents'
 import UserEdit from './components/UserEdit'
+import EventPage from './components/EventPage'
+import Events from './components/Events'
 
 function App() {
   return (
@@ -17,6 +19,13 @@ function App() {
         <Route path = '/user/id' element = {<User/>}/>
         <Route path = '/user/id/events' element = {<UserEvents/>}/>
         <Route path = '/user/id/edit' element = {<UserEdit/>}/>
+      </Routes>
+
+      <Routes>
+        <Route path ='/events' element={<Events/>}/>
+        <Route path ='/event/id' element = {<EventPage/>}/>
+
+
       </Routes>
     </Router>
     </>
