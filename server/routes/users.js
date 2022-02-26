3eac009ed6d2f59395d42719bc6ee49eeff135c2
dowhep <caogangshen@gmail.com>
2022-02-26 10:57:51 -0800

    Fixed the routes organization and figured out how to host server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Event = require("../models/event.model");
-const User = require("../models/user.model");
+const User = require("../models/user");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -13,20 +12,7 @@ router.get("/", function (req, res, next) {
   res.status(200).json({ user });
 });
 
-router.put("/events/:id", async function (req, res) {
-  try {
-    const { id, update } = req.params;
-    const event = await Event.findByIdAndUpdate(id, update);
-    if (!event) {
-      return res.status(404).json({ error: "Event does not exist", id });
-    }
-    res.status(200).json({ event });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-});
-
-router.put("/users/:id", async function (req, res) {
+router.put("/:id", async function (req, res) {
   try {
     const { id, update } = req.params;
     const user = await User.findByIdAndUpdate(id, update);
@@ -39,20 +25,7 @@ router.put("/users/:id", async function (req, res) {
   }
 });
 
-router.delete("/events/:id", async function (req, res) {
-  try {
-    const { id } = req.params;
-    const event = await Event.findByIdAndDelete(id);
-    if (!event) {
-      return res.status(404).json({ error: "Event does not exist", id });
-    }
-    res.status(200).json({ event });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-});
-
-router.delete("/users/:id", async function (req, res) {
+router.delete("/:id", async function (req, res) {
   try {
     const { id } = req.params;
     const user = await User.findByIdAndDelete(id);
