ac7c59251b898b2fa2b97bf8de26053afe8821e4
j1phan <j1phan@ucsd.edu>
2022-03-03 16:16:50 -0800

    routes

---
+++ client/src/components/EventPage.js
@@ -0,0 +1,21 @@
+import React,{ useState, useEffect } from 'react'
+
+export const EventPage = () => {
+    useEffect(() => {
+
+      });
+
+    return (
+    <div className = 'containerEvent'>
+        <div className = 'eventPage'>
+            <div className = 'eventPageTitle'>Event</div>
+        </div>
+        <div className = 'backToEventTimeline'>
+            <a href='/events'> Back to Event Timeline</a>
+        </div>
+    </div>
+  )
+}
+
+
+export default EventPage 
