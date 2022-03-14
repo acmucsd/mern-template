12d4f4f971ac2c78d989620ab0ea89dc816b074e
kinana52 <93495806+kinana52@users.noreply.github.com>
2022-03-13 22:59:55 -0700

    S3 picture uploads

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { upload } = require('../storage');
 
 const User = require("../models/user");
+
+const multer  = require('multer')
+const fileSizeLimitInBytes = 2 * 1024 * 1024;
+const storage = multer({ storage: multer.memoryStorage(), limits: {fileSize: fileSizeLimitInBytes} })
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -22,7 +27,7 @@ router.post("/", async function (req, res) {
 router.put("/:id", async function (req, res) {
   try {
     const { id, update } = req.params;
-    const user = await User.findByIdAndUpdate(id, update);
+    const user = await User.findByIdAndUpdate(id, update, {new: true});
     if (!user) {
       return res.status(404).json({ error: "User does not exist", id });
     }
@@ -43,6 +48,19 @@ router.delete("/:id", async function (req, res) {
   } catch (error) {
     res.status(500).json({ error });
   }
+});
+
+//UPLOAD PICTURE
+router.put("/:id/picture", storage.single('image'), async function (req, res) {
+  const id = req.params.id;
+  const potentialUser = await User.findById(id);
+  if(!potentialUser) {
+    return res.status(404).json({error: "User does not exist", id });
+  }
+  const profilePicture = await upload(req.file, id);
+  console.log(profilePicture);
+  const user = await User.findByIdAndUpdate(id, {pfp: profilePicture}, {new: true});
+  return res.status(200).json({ user });
 });
 
 module.exports = router;
