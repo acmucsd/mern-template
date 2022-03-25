dc9d196365e6aa8f201fbfc1253773046eba2933
dowhep <caogangshen@gmail.com>
2022-03-24 18:14:11 -0700

    implemented the login route and hashed the user password before storing it

--- server/config/index.js
+++ server/config/index.js
@@ -8,7 +8,7 @@ const config = {
   S3_REGION: process.env.S3_REGION,
   S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
   S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
-  S3_BUCKET: process.env.S3_BUCKET
+  S3_BUCKET: process.env.S3_BUCKET,
 };
 
 module.exports = config;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { upload } = require('../storage');
+const { upload } = require("../storage");
 
 const User = require("../models/user");
 
-const multer  = require('multer')
+const multer = require("multer");
 const fileSizeLimitInBytes = 2 * 1024 * 1024;
-const storage = multer({ storage: multer.memoryStorage(), limits: {fileSize: fileSizeLimitInBytes} })
+const saltRounds = 7;
+const storage = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: fileSizeLimitInBytes },
+});
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
@@ -21,7 +25,29 @@ router.get("/", function (req, res, next) {
 router.post("/", async function (req, res) {
   const { user } = req.body;
   const newUser = await User.create(user);
+  newUser.password = await bcrypt.hash(newUser.password, saltRounds);
   res.status(200).json({ newUser });
+});
+
+router.post("/login", async function (req, res) {
+  try {
+    const { email, password } = req.body;
+    const potentialUser = await User.findOne({ email });
+    if (!potentialUser) {
+      return res
+        .status(404)
+        .json({ error: "Email does not exist", email, password });
+    }
+    const isMatch = await bcrypt.compare(password, potentialUser.password);
+    if (!isMatch) {
+      return res
+        .status(404)
+        .json({ error: "Password does not match", email, password });
+    }
+    res.status(200).json({ potentialUser });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 router.put("/:id", async function (req, res) {
@@ -52,15 +78,19 @@ router.delete("/:id", async function (req, res) {
 });
 
 //UPLOAD PICTURE
-router.put("/:id/picture", storage.single('image'), async function (req, res) {
+router.put("/:id/picture", storage.single("image"), async function (req, res) {
   const id = req.params.id;
   const potentialUser = await User.findById(id);
-  if(!potentialUser) {
-    return res.status(404).json({error: "User does not exist", id });
+  if (!potentialUser) {
+    return res.status(404).json({ error: "User does not exist", id });
   }
   const profilePicture = await upload(req.file, id);
   console.log(profilePicture);
-  const user = await User.findByIdAndUpdate(id, {pfp: profilePicture}, {new: true});
+  const user = await User.findByIdAndUpdate(
+    id,
+    { pfp: profilePicture },
+    { new: true }
+  );
   return res.status(200).json({ user });
 });
 
